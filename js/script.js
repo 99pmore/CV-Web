3837ab34570f4db2ab710911b45bf79ee56ef213
99pmore <morenomartin.pablo@gmail.com>
2022-08-15 14:27:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const toggleMenu = () => {
+    document.getElementById("menu-mob").classList.toggle("menu-open");
+    document.getElementById("burger").classList.toggle("is-active");
+    document.getElementById("menu-bg").classList.toggle("visible");
+  };
